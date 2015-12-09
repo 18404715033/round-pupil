ad7e129318a3a499bda66a599c015144e1b9cb81
18404715033 <965630447@qq.com>
2015-12-09 19:11:04 +0800

    简单的计算圆的周长和体积
    声明常量（const变量）pi为3.1415926，
    声明3个变量，计算圆的周长，计算圆的面积。

--- round pupil/main.cpp
+++ round pupil/main.cpp
@@ -7,9 +7,14 @@
 //
 
 #include <iostream>
-
+using namespace std;
+const double PI=3.1415926;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    double r,l,s;
+    cin>>r;
+    l=2*PI*r;
+    cout<<"l="<<l<<endl;
+    s=PI*r*r;
+    cout<<"s="<<s<<endl;
     return 0;
 }
